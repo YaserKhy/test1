65e1e9795d91ca804349c7f22c58215fffb45f44
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-07-31 11:17:51 +0300

    adding new hello

--- bin/test1.dart
+++ bin/test1.dart
@@ -3,3 +3,7 @@ import 'package:test1/test1.dart' as test1;
 void main(List<String> arguments) {
   print('Hello world: ${test1.calculate()}!');
 }
+
+String hello2() {
+  return "hello one more time !";
+}
